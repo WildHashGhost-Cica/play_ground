95cbac8abbe15cbab5abfceb4ff7659ae79d1144
WildHashGhost-Cica <wildhashghost@gmail.com>
2022-09-28 16:36:56 +0100

    method, function, related Function

---
+++ src/bin/function.rs
@@ -0,0 +1,43 @@
+#[derive(Debug)]
+
+struct Object {
+    width: u32,
+    height: u32,
+}
+//Methods 
+impl Object {
+    fn area(&self) -> u32{
+        self.width * self.height
+    
+    }
+
+    fn new(width: u32, height: u32) -> Object{
+        Object{
+            width,
+            height,
+        }
+    }
+}
+//Related function
+impl Object {
+    fn show(&self){
+        println!("{} x {} with area: {} ", self.width, self.height, self.area());
+    }
+}
+
+fn main(){
+    let o = Object{
+        width: 35,
+        height: 55,
+    };
+
+    let obj = Object::new(57, 83);
+
+   
+    o.show();
+    obj.show();
+
+    println!("{:?}",o);
+    println!("{:?}",obj);
+
+}
