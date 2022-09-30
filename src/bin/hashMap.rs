74da579d467895a1bc221826e282aacd067c92f0
WildHashGhost-Cica <wildhashghost@gmail.com>
2022-09-30 18:45:27 +0100

    hashMap

---
+++ src/bin/hashMap.rs
@@ -0,0 +1,19 @@
+use std::collections::HashMap;
+
+fn main(){
+    let mut hm = HashMap::new();
+
+    hm.insert(String::from("random"), 12);
+    hm.insert(String::from("string"), 49);;
+    //remove string
+    hm.remove(&String::from("string"));
+
+    for (k, v) in &hm {
+        println!("{}: {}", k, v);
+    }
+
+    match hm.get(&String::from("random")){
+        Some(&n) => println!("{}", n),
+        _ => println!("no match"),
+    }
+}
