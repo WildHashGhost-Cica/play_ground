8f496517a03a2c186c2a01e0db44a450567b7a31
WildHashGhost-Cica <wildhashghost@gmail.com>
2022-10-01 10:45:00 +0100

    while let

---
+++ src/bin/while_let.rs
@@ -0,0 +1,22 @@
+fn main(){
+    let mut n = Some(0);
+
+    //replace loop wiht while let 
+    /*
+    loop {
+        match n {
+            Some(i) => if i >19 {
+                println!("Quit");
+                n = None;
+            }else{
+                println!("{}", i);
+                n = Some(i +2);
+            },
+            _ => {
+                break;
+            }
+        }
+    }*/
+
+    
+}
