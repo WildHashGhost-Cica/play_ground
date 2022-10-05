dc53dc09b6d9a56107c7ef65b9b72875dbc4e953
WildHashGhost-Cica <wildhashghost@gmail.com>
2022-10-05 10:13:36 +0100

    closures

---
+++ src/bin/closures_03.rs
@@ -0,0 +1,22 @@
+fn run<F>(f: F)
+where F: Fn() {
+    f();
+}
+
+fn add3<F>(f:F) -> i32
+where F: Fn(i32) -> i32 {
+    f(3)
+}
+
+
+
+fn main(){
+    let p = || println!("hello from run function!");
+    run(p);
+
+    let x = |i| i * 10;
+
+    println!("3 * 10 = {}", add3(x));
+
+   
+}
