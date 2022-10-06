3c862c54c618c1186d3099b123f9b39a63fb8bc0
WildHashGhost-Cica <wildhashghost@gmail.com>
2022-10-06 11:19:18 +0100

    thread

---
+++ src/bin/thread_04.rs
@@ -0,0 +1,10 @@
+use std::thread;
+use std::sync::mpsc;
+
+fn main(){
+    let (tx, rx) = mpsc::channel();
+
+    thread::spawn(move || { tx.send(42).unwrap();});
+
+    println!("got {}", rx.recv().unwrap());
+}
