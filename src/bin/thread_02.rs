aa879fc555959406628eb222c5569d06cdc0bf65
WildHashGhost-Cica <wildhashghost@gmail.com>
2022-10-06 10:43:37 +0100

    thread

---
+++ src/bin/thread_02.rs
@@ -0,0 +1,15 @@
+use std::thread;
+
+fn main(){
+    let mut c = vec![];
+
+    for i in  0..10{
+        c.push(thread::spawn(move || {
+            println!("thread number{}",i);
+        }));
+    }
+
+    for j in c {
+        j.join();
+    }
+}
