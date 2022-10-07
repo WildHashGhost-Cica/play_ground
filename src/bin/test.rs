bef44d69e7e23c3d08b713c3f6aed87c545a310e
WildHashGhost-Cica <wildhashghost@gmail.com>
2022-10-07 14:26:48 +0100

    test

---
+++ src/bin/test.rs
@@ -0,0 +1,16 @@
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works(){
+    }
+
+    #[test]
+    fn check_two(){
+        assert!(1 + 1 == 2);
+    }
+        #[test]
+        fn test_three(){
+            assert!(3 == 4);
+        }
+    
+}
